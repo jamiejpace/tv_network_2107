738db02641c3e3898e97b8f24ab6c122f3ddfac9
Jamie Pace <jamiejpace@gmail.com>
2021-07-21 10:21:26 -0600

    Add show class and test

--- lib/show.rb
+++ lib/show.rb
@@ -1,1 +1,28 @@
+class Show
+  attr_reader :name, :creator, :characters
 
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.map do |character|
+      character.salary
+    end.sum
+  end
+
+  def highest_paid_actor
+    highest_salary = @characters.max_by do |character|
+      character.salary
+    end
+    highest_salary.actor
+  end
+
+  def actors
+    @characters.map do |character|
+      character.actor
+    end
+  end
+end
